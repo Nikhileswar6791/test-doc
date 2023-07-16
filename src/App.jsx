d9ff73c54598d89a20f05c4017eec3c1edd93654
unknown <nikhileswarreddypochimireddy@gmail.com>
2023-07-16 16:18:55 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/Header";
+import Note from "./components/Note";
+function App(){
+    return <div>
+        <Header/>
+        <Note/>
+    </div>;
+}
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,9 @@
+function Note(){
+    return <div className="card">
+    <div className="card-body">
+      This is some text within a card body.ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+    </div>
+  </div>
+}
+
+export default Note;
